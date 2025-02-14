f7af537a6e745fdebdadc10a4a2b37dcbfe4b785
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-02-14 18:51:23 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Feb-2025/Books 266253.py
@@ -0,0 +1,16 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+n,m=map(int, input().split())
+nums = list(map(int, input().split()))
+
+summ = 0
+left = 0
+res = 0
+for right in range (len(nums)):
+    summ += nums[right]
+
+    if summ > m:
+        summ -= nums[left]
+        left +=1
+    res = max(res, right - left + 1 )
+print(res)
