59a81728a37db3349074c6426550529aab6d3203
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-02-16 23:42:02 +0300

    Created 'A - Make the Product Zero' by A2SV Hub

---
+++ A2SV G6 - Round #2 16-Feb-2025/A - Make the Product Zero 267995.py
@@ -0,0 +1,9 @@
+# Problem: A - Make the Product Zero - https://codeforces.com/gym/586960/problem/A
+
+t=int(input())
+nums=list(map(int, input().split()))
+
+minimum=[]
+for num in nums:
+    minimum.append(abs(num))
+print(min(minimum))
