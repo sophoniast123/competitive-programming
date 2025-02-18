5c6e80879b21644aa7b43e8206bd28a06f5c9ad8
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-02-18 17:56:09 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 18-Feb-2025/E - Equalizing Arrays 272114.py
@@ -0,0 +1,36 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+import sys
+n = int(input())
+nums1 = list(map(int, input().split()))
+m = int(input())
+nums2 = list(map(int, input().split()))
+
+if sum(nums1) != sum(nums2):
+    print(-1)
+    sys.exit(0)
+
+count = 0
+prefix_1 = [nums1[0]]*len(nums1)
+prefix_2 = [nums2[0]]*len(nums2)
+
+for i in range(1,len(nums1)):
+    prefix_1 [i] = prefix_1[i - 1] + nums1 [i]
+
+for i in range(1,len(nums2)):
+    prefix_2 [i] = prefix_2[i - 1] + nums2 [i]
+
+l, r = 0, 0
+while l < len(prefix_1) and r < len(prefix_2):
+    if prefix_1[l] == prefix_2[r]:
+        l += 1
+        r += 1
+        count += 1
+    elif prefix_1[l] < prefix_2[r]:
+        l += 1
+    else:
+        r += 1
+if count > 0:
+    print(count)
+else:
+    print(-1)
