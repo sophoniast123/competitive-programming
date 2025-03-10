db4c4f240e931c73f30a7e9507df48e7820ca18a
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-03-10 16:52:46 +0300

    Created 'Odd Subarrays' by A2SV Hub

---
+++ 10-Mar-2025/Odd Subarrays 300315.py
@@ -0,0 +1,18 @@
+# Problem: Odd Subarrays - https://codeforces.com/problemset/problem/1686/B
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+    max_inv = 0
+    count = 0
+    for num in arr:
+        if max_inv > num:
+            count += 1
+            max_inv = 0
+        else:
+            max_inv = max(max_inv,num)
+    print(count)
+        
