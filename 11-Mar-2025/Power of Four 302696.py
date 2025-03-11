a66390c89ddd10a2dcd3eadd946d512fca7adb9f
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-03-11 18:04:29 +0300

    Created 'Power of Four' by A2SV Hub

---
+++ 11-Mar-2025/Power of Four 302696.py
@@ -0,0 +1,12 @@
+# Problem: Power of Four - https://leetcode.com/problems/power-of-four/
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+       
+        if n <= 0:
+            return False 
+
+        while n % 4 == 0:
+            n //= 4
+        return abs(n) == 1
+            
