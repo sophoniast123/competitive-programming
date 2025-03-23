196120bb39172c7d20b0eedc72887f6e9bc7236d
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-03-24 00:42:17 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 23-Mar-2025/Karen and Coffee 316442.py
@@ -0,0 +1,35 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+n, k, q = map(int, input().split())
+
+recipes = []
+for _ in range(n):
+    recipe = tuple(map(int, input().split()))
+    recipes.append(recipe)
+    
+questions = []
+for _ in range(q):
+    question = tuple(map(int, input().split()))
+    questions.append(question)
+    
+recipe_prefix = [0] * (200000 + 2)   # 200000 is its constraint
+
+for recipe in recipes:
+    recipe_prefix[recipe[0]] += 1
+    recipe_prefix[recipe[1] + 1] -= 1
+    
+for i in range(1, len(recipe_prefix)):
+    recipe_prefix[i] += recipe_prefix[i - 1]
+    
+for i in range(len(recipe_prefix)):
+    if recipe_prefix[i] >= k:
+        recipe_prefix[i] = 1
+    else:
+        recipe_prefix[i] = 0
+
+for i in range(1, len(recipe_prefix)):
+    recipe_prefix[i] += recipe_prefix[i - 1]
+    
+for question in questions:
+    count = recipe_prefix[question[1]] - recipe_prefix[question[0] - 1]
+    print(count, sep="\n")
