b000d14b5313822209b18d804cc76764d0c01166
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-03-26 01:26:49 +0300

    Created 'Array Splitting' by A2SV Hub

---
+++ 25-Mar-2025/Array Splitting 318458.py
@@ -0,0 +1,13 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+n,k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+
+difference = []
+for i in range(1, n):
+    difference.append(arr[i]-arr[i-1])
+
+difference.sort()
+
+print(sum(difference[:n - k]))
