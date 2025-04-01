9d21559dfcc1b6e45b23028d23efc0af52f22108
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-04-01 23:52:07 +0300

    Created 'Count Subarrays With Fixed Bounds' by A2SV Hub

---
+++ 01-Apr-2025/Count Subarrays With Fixed Bounds 323651.py
@@ -0,0 +1,16 @@
+# Problem: Count Subarrays With Fixed Bounds - https://leetcode.com/problems/count-subarrays-with-fixed-bounds/
+
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        j1 = j2 = k = -1
+        ans = 0
+        for i, v in enumerate(nums):
+            if v < minK or v > maxK:
+                k = i
+            if v == minK:
+                j1 = i
+            if v == maxK:
+                j2 = i
+            ans += max(0, min(j1, j2) - k)
+        return ans
+        
