60379756ece0974533d3b53e891ba6b7ead047a4
Sophonias Tamene Seifu <136117376+sophoniast123@users.noreply.github.com>
2025-04-20 16:10:17 +0300

    Created 'A - Escaping Prison' by A2SV Hub

---
+++ A2SV G6 - Round #9 | Adwa Special 20-Apr-2025/A - Escaping Prison 337690.py
@@ -0,0 +1,18 @@
+# Problem: A - Escaping Prison - https://codeforces.com/gym/601269/problem/A
+
+t = int(input())
+
+
+for _ in range(t):
+    n, h = map(int, input().split())
+    arr = []
+    for _ in range(n):
+        w, l = map(int, input().split())
+
+        arr.append(max(w,l))
+        
+
+    if sum(arr) >= h:
+        print('YES')
+    else:
+        print('NO')
